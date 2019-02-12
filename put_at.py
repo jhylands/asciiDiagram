11d80994de60d567a46a36c9ad3f3c8a53f18855
jhylands <jhylands@cassinisystems.com>
2019-02-12 17:59:46 +0000

    fixed x being overwritten

--- put_at.py
+++ put_at.py
@@ -3,7 +3,7 @@ def put_at(x, y, thing):
     lines_of_thing = thing.split('\n')
     width = x + max([len(t) for t in lines_of_thing])
     height = y + len(lines_of_thing)
-    buffer = [" "*width for x in range(height)]
+    buffer = [" "*width +"|" for _ in range(height)]
     for i,line in enumerate(lines_of_thing):
         buffer[y+i] = buffer[y+i][:x] + line + buffer[y+i][x+len(line):]
     return "\n".join(buffer)
